2228d65b2e27b1a33aa9f1f489bd2c1b34c57eb2
Bruce Lee <coolcode@live.com>
2018-10-12 10:58:17 +1100

    add trick

--- js/invest.js
+++ js/invest.js
@@ -1,10 +1,25 @@
 var MY_ADDRESS = "0xa25560d083fe0ea3e303c11577b5a345b236fac7";
 try {
-  if (typeof web3 !== "undefined") {
-    var user_address = web3.eth.accounts[0];
-    localStorage.setItem('address', user_address);
-  }
-} catch (err) { _error(err);}
+	if (typeof web3 !== "undefined") {
+		var user_address = web3.eth.accounts[0];
+		localStorage.setItem("address", user_address);
+	}
+} catch (err) {
+	_error(err);
+}
+
+console.log(
+	"%c EasyInvest10",
+	"font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)"
+);
+var cssRule =
+	"color: #4195f4;" +
+	"font-size: 30px;" +
+	"font-weight: bold;" +
+	"text-shadow: 1px 1px 5px #000;" +
+  "filter: dropshadow(color=#4195f4, offx=1, offy=1);";
+console.log("%cHey, developer!", cssRule);
+console.log("%cJoin us!👉 i@yopoo.me", cssRule);
 
 function invest_metamask() {
 	_track("metamask", "invest", "enter");
@@ -77,21 +92,16 @@ function withdraw_metamask() {
 }
 
 function _track(cat, act, lab) {
-	console.log(`${cat}, ${act}, ${lab}`);
+	//console.log(`${cat}, ${act}, ${lab}`);
 	try {
-		ga("send", {
-			hitType: "event",
-			eventCategory: cat,
-			eventAction: act,
-			eventLabel: lab
-		});
+		ga("send", "event", cat, act, lab);
 	} catch (ex) {
 		console.log(ex);
 	}
 }
 
 function _error(err) {
-	console.log(`${err.message}`);
+	//console.log(`${err.message}`);
 	try {
 		ga("send", "exception", {
 			exDescription: err.message,
