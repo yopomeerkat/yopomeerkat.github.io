bf86fa50ee566325f3af00c08ae8b08396ec824c
Bruce Lee <coolcode@live.com>
2018-10-12 07:41:56 +1100

    :monkey_face: fix issue: 1.cannot search stat by passing a url params, 2.cal payouts, 3.stat click events

--- js/info.js
+++ js/info.js
@@ -1,55 +1,74 @@
-﻿function getInfo(initialInfo){
-
-
-	var rate = fetch("https://api.coinmarketcap.com/v1/ticker/ethereum/")
-		.then(function(response){
+﻿function getInfo(initialInfo) {
+	var rate = fetch("https://api.coinmarketcap.com/v1/ticker/ethereum/").then(
+		function(response) {
 			return response.json();
-		});
+		}
+	);
 
-	if(!initialInfo)
-		initialInfo = {sum: 0, timesum: 0, num: 0, investors: {}, dates: [], nums: [], sums: [], min: -1, max: 0};
+	if (!initialInfo)
+		initialInfo = {
+			sum: 0,
+			timesum: 0,
+			num: 0,
+			investors: {},
+			dates: [],
+			nums: [],
+			sums: [],
+			min: -1,
+			max: 0
+		};
 
-	var jsonInternalPromise = fetch("https://api.etherscan.io/api?module=account&action=txlistinternal&address=" + initialInfo.address + "&startblock=" + ((initialInfo.lastBlock || 0) + 1) + "&endblock=99999999&sort=asc&apikey=YourApiKeyToken")
-		.then(function(response){
+	var jsonInternalPromise = fetch(
+		"https://api.etherscan.io/api?module=account&action=txlistinternal&address=" +
+			initialInfo.address +
+			"&startblock=" +
+			((initialInfo.lastBlock || 0) + 1) +
+			"&endblock=99999999&sort=asc&apikey=YourApiKeyToken"
+	).then(function(response) {
+		return response.json();
+	});
+
+	return fetch(
+		"https://api.etherscan.io/api?module=account&action=txlist&address=" +
+			initialInfo.address +
+			"&startblock=" +
+			((initialInfo.lastBlock || 0) + 1) +
+			"&endblock=99999999&sort=asc&apikey=YourApiKeyToken"
+	)
+		.then(function(response) {
 			return response.json();
-		});
-
-	return fetch("https://api.etherscan.io/api?module=account&action=txlist&address=" + initialInfo.address + "&startblock=" + ((initialInfo.lastBlock || 0) + 1) + "&endblock=99999999&sort=asc&apikey=YourApiKeyToken")
-		.then(function(response){
-			return response.json();
-		}).then(function(json){
-			var info = json.result.reduce(function(info, tr){
+		})
+		.then(function(json) {
+			var info = json.result.reduce(function(info, tr) {
 				var val = +tr.value;
-				if(!info.firstBlock){
+				if (!info.firstBlock) {
 					info.firstBlock = +tr.blockNumber;
 					info.firstTime = +tr.timeStamp;
 				}
 				info.lastBlock = +tr.blockNumber;
 				info.lastTime = +tr.timeStamp;
 
-				if(info.investors[tr.from]){
+				if (info.investors[tr.from]) {
 					info.investors[tr.from].gas += tr.gasUsed * tr.gasPrice;
-				}else{
+				} else {
 					info.investors[tr.from] = {
 						sum: 0,
 						inv: [],
 						gas: tr.gasUsed * tr.gasPrice
-					}
+					};
 				}
 
-				if(+tr.isError)
-					return info;
-				
-				if(val){
+				if (+tr.isError) return info;
+
+				if (val) {
 					info.last = val;
 					info.last_time = tr.timeStamp * 1000;
-			
+
 					info.sum += val;
-					info.timesum += val*tr.blockNumber;
+					info.timesum += val * tr.blockNumber;
 
 					var inv = info.investors[tr.from];
-					if(!inv.sum)
-						info.num += 1;
+					if (!inv.sum) info.num += 1;
 
 					inv.sum += val;
 					inv.inv.push({
@@ -58,55 +77,60 @@
 					});
 
 					var investment = info.investors[tr.from].sum;
-					
+
 					info.dates.push(info.last_time);
 					info.nums.push(info.num);
-					info.sums.push(Math.round(info.sum/Math.pow(10,16))/100);
-			
-					if(info.min == -1 || info.min > investment)
+					info.sums.push(
+						Math.round(info.sum / Math.pow(10, 16)) / 100
+					);
+
+					if (info.min == -1 || info.min > investment)
 						info.min = investment;
-					if(investment > info.max)
-						info.max = investment;
+					if (investment > info.max) info.max = investment;
 				}
 				return info;
 			}, initialInfo);
 
-			info.avg = info.sum/info.num;
+			info.avg = info.sum / info.num;
 
-			return rate.then(function(ratejson){
+			return rate.then(function(ratejson) {
 				info.rate = +ratejson[0].price_usd;
-				info.sum_usd = info.sum/Math.pow(10, 18) * info.rate;
+				info.sum_usd = (info.sum / Math.pow(10, 18)) * info.rate;
 
-				jsonInternalPromise.then(function(json){
-					info = json.result.reduce(function(info, tr){
-						if(!(+tr.isError)){
-							var inv = info.investors[tr.to];
-							if(inv){
-								inv.got = (inv.got || 0) + (+tr.value);
-								inv.gotTime = tr.timeStamp*1000;
+				jsonInternalPromise.then(
+					function(json) {
+						info = json.result.reduce(function(info, tr) {
+							if (!+tr.isError) {
+								var inv = info.investors[tr.to];
+								if (inv) {
+									inv.got = (inv.got || 0) + +tr.value;
+									inv.gotTime = tr.timeStamp * 1000;
+								}
+								info.got = (info.got || 0) + +tr.value;
 							}
-						    info.got = (info.got || 0) + (+tr.value);
-						}
+							return info;
+						}, info);
+					},
+					function(e) {
+						console.log(
+							"Error fetching internal transactions: " + e
+						);
 						return info;
-					}, info);
-				}, function(e){
-					console.log('Error fetching internal transactions: ' + e);
-					return info;
-				});
+					}
+				);
 
 				return info;
 			});
-
 		});
 }
 
-function drawChart(info){
-    if (document.getElementById('chart-container2')) {
-    	var avg = (Math.round(info.avg/Math.pow(10,16))/100);
-    	var min = (Math.ceil(info.min/Math.pow(10,16))/100);
-    	var max = (Math.round(info.max/Math.pow(10,16))/100);
-        Highcharts.chart('chart-container2', {
-            /*tooltip: {
+function drawChart(info) {
+	if (document.getElementById("chart-container2")) {
+		var avg = Math.round(info.avg / Math.pow(10, 16)) / 100;
+		var min = Math.ceil(info.min / Math.pow(10, 16)) / 100;
+		var max = Math.round(info.max / Math.pow(10, 16)) / 100;
+		Highcharts.chart("chart-container2", {
+			/*tooltip: {
               /*  formatter: function () {
     
                     if (this.series.name == 'AVG') {
@@ -117,271 +141,301 @@ function drawChart(info){
                     }
                 }
             }, */
-            title: {
-                text: 'Combination chart',
-                style: {
-                	color: (Highcharts.theme && Highcharts.theme.textColor) || 'white'
-                }
-            },
-            xAxis: {
-                type: 'datetime',
-                dateTimeLabelFormats: { // don't display the dummy year
-    				day: '%e %b',
-    				week: '%e %b',
-                    month: '%e %b',
-                    year: '%e %b'
-                },
-                title: {
-                    enabled: false
-                }
-            },
-            labels: {
-                items: [{
-                    html: 'Average ' + avg + 'ETH, max ' + max + ' ETH',
-                    style: {
-                        left: '50px',
-                        top: '18px',
-                        color: (Highcharts.theme && Highcharts.theme.textColor) || 'white'
-                    }
-                }]
-            },
-            legend: {
-                itemStyle: {
-                    color: '#A0A0A0'
-                },
-                itemHoverStyle: {
-                    color: '#FFF'
-                },
-                itemHiddenStyle: {
-                    color: '#444'
-                }
-            },
-            yAxis: [{
-                labels: {
-                    enabled: true
-                },
-                title: {
-                    enabled: false
-                },
-                minorGridLineWidth: 0.2,
-                gridLineWidth: 0.1,
-                alternateGridColor: null,
-            }],
-    
-            plotOptions: {
-                column: {},
-                spline: {
-                    dashStyle: 'Dot',
-                    marker: {enabled: false},
-                    states: {hover: {enabled: true}},
-                },
-                area: {
-                    marker: {enabled: false},
-                    states: {hover: {enabled: true}},
-                    style: 'dotted',
-                    fillOpacity: 0.2,
-                },
-                pie: {}
-    
-            },
-    
-            series: [
-            	{ //Line
-                    type: 'area',
-                    name: 'ETH',
-    
-                    data: info.sums.map(function(e, i) { return [info.dates[i], e] }),
-                    marker: {
-                        lineWidth: 2,
-                        lineColor: Highcharts.getOptions().colors[3],
-                        fillColor: 'white'
-                    },
-                    enableMouseTracking: true,
-    
-                    color: '#ee06a4',
-                    shadow: {
-                        color: '#ee06a4',
-                        width: 3,
-                        offsetX: 0,
-                        offsetY: 0
-                    }
-                }, { // Line 2
-                    type: 'spline',
-                    name: 'Users',
-                    data: info.nums.map(function(e, i) { return [info.dates[i], e] }),
-                    color: '#78ee06',
-                    enableMouseTracking: true,
-                    shadow: {
-                        color: '#78ee06',
-                        width: 3,
-                        offsetX: 0,
-                        offsetY: 0
-                    },
-                    formatter: function () {
-                        return this.value;
-                    }
-                },
-                {
-                    type: 'pie',
-                    name: 'Investment',
-                    data: [
-                        {
-                            name: avg + ' AVG',
-                            sliced: true,
-                            selected: true,
-                            y: avg,
-                            color: 'rgba(150,100,50,0.1)' // AVG color
-                        }, {
-                            name: min + ' MIN',
-                            y: min,
-                            color: 'rgba(200,122,200,0.7)' // Joe's color
-                        },
-                        {
-                            name: max + ' MAX',
-                            y: max,
-                            color: 'rgba(200,122,200,1)' // John's color
-                        },
-                    ],
-                    center: [100, 80],
-                    size: 100,
-                    showInLegend: false,
-                    dataLabels: {
-                        enabled: false
-                    }
-                }]
-        });
-    }
+			title: {
+				text: "Combination chart",
+				style: {
+					color:
+						(Highcharts.theme && Highcharts.theme.textColor) ||
+						"white"
+				}
+			},
+			xAxis: {
+				type: "datetime",
+				dateTimeLabelFormats: {
+					// don't display the dummy year
+					day: "%e %b",
+					week: "%e %b",
+					month: "%e %b",
+					year: "%e %b"
+				},
+				title: {
+					enabled: false
+				}
+			},
+			labels: {
+				items: [
+					{
+						html: "Average " + avg + "ETH, max " + max + " ETH",
+						style: {
+							left: "50px",
+							top: "18px",
+							color:
+								(Highcharts.theme &&
+									Highcharts.theme.textColor) ||
+								"white"
+						}
+					}
+				]
+			},
+			legend: {
+				itemStyle: {
+					color: "#A0A0A0"
+				},
+				itemHoverStyle: {
+					color: "#FFF"
+				},
+				itemHiddenStyle: {
+					color: "#444"
+				}
+			},
+			yAxis: [
+				{
+					labels: {
+						enabled: true
+					},
+					title: {
+						enabled: false
+					},
+					minorGridLineWidth: 0.2,
+					gridLineWidth: 0.1,
+					alternateGridColor: null
+				}
+			],
 
+			plotOptions: {
+				column: {},
+				spline: {
+					dashStyle: "Dot",
+					marker: { enabled: false },
+					states: { hover: { enabled: true } }
+				},
+				area: {
+					marker: { enabled: false },
+					states: { hover: { enabled: true } },
+					style: "dotted",
+					fillOpacity: 0.2
+				},
+				pie: {}
+			},
+
+			series: [
+				{
+					//Line
+					type: "area",
+					name: "ETH",
+
+					data: info.sums.map(function(e, i) {
+						return [info.dates[i], e];
+					}),
+					marker: {
+						lineWidth: 2,
+						lineColor: Highcharts.getOptions().colors[3],
+						fillColor: "white"
+					},
+					enableMouseTracking: true,
+
+					color: "#ee06a4",
+					shadow: {
+						color: "#ee06a4",
+						width: 3,
+						offsetX: 0,
+						offsetY: 0
+					}
+				},
+				{
+					// Line 2
+					type: "spline",
+					name: "Users",
+					data: info.nums.map(function(e, i) {
+						return [info.dates[i], e];
+					}),
+					color: "#78ee06",
+					enableMouseTracking: true,
+					shadow: {
+						color: "#78ee06",
+						width: 3,
+						offsetX: 0,
+						offsetY: 0
+					},
+					formatter: function() {
+						return this.value;
+					}
+				},
+				{
+					type: "pie",
+					name: "Investment",
+					data: [
+						{
+							name: avg + " AVG",
+							sliced: true,
+							selected: true,
+							y: avg,
+							color: "rgba(150,100,50,0.1)" // AVG color
+						},
+						{
+							name: min + " MIN",
+							y: min,
+							color: "rgba(200,122,200,0.7)" // Joe's color
+						},
+						{
+							name: max + " MAX",
+							y: max,
+							color: "rgba(200,122,200,1)" // John's color
+						}
+					],
+					center: [100, 80],
+					size: 100,
+					showInLegend: false,
+					dataLabels: {
+						enabled: false
+					}
+				}
+			]
+		});
+	}
 }
 
-function findAddress(address){
+function findAddress(address) {
 	var addr = address.toLowerCase();
-	if(!window.investmentInfo)
-		return null;
+	if (!window.investmentInfo) return null;
 
-    if(investmentInfo.investors[addr])
-    	return addr;
+	if (investmentInfo.investors[addr]) return addr;
 
-    for(var key in investmentInfo.investors){
-    	if(key.indexOf(addr) === 0)
-    		return key;
-    }
+	for (var key in investmentInfo.investors) {
+		if (key.indexOf(addr) === 0) return key;
+	}
 
-    return null;
+	return null;
 }
 
-function updateDividentsTimer(investmentInfo){
-	if(window.g_dividentsTimer){
+function updateDividentsTimer(investmentInfo) {
+	if (window.g_dividentsTimer) {
 		window.clearInterval(window.g_dividentsTimer);
 		window.g_dividentsTimer = 0;
 	}
 
-	if(investmentInfo){
-        updateDividents(investmentInfo, 'Now');
-        updateDividents(investmentInfo, 'Month');
-        
-		window.g_dividentsTimer = window.setInterval(function(){
-			updateDividents(investmentInfo, 'Now');
+	if (investmentInfo) {
+		updateDividents(investmentInfo, "Now");
+		updateDividents(investmentInfo, "Month");
+
+		window.g_dividentsTimer = window.setInterval(function() {
+			updateDividents(investmentInfo, "Now");
 		}, 1000);
-	}else{
-		setCalcValues('?', 'Month', '?', '?', '?');
-		setCalcValues('?', 'Now', '?', '?', '?');
+	} else {
+		setCalcValues("?", "Month", "?", "?", "?");
+		setCalcValues("?", "Now", "?", "?", "?");
 	}
 }
 
-function updateDividents(investmentInfo, type){
-
+function updateDividents(investmentInfo, type) {
 	var percent = 10;
 	var curTime = +new Date();
-	if(type !== 'Month'){
+	if (type !== "Month") {
 		var divs = 0;
 		var sum = 0;
-		for(var i=0; i<investmentInfo.inv.length; ++i){
+		for (var i = 0; i < investmentInfo.inv.length; ++i) {
 			var inv = investmentInfo.inv[i];
 			sum += inv.sum;
-			divs += inv.sum * (curTime - Math.max(inv.time, investmentInfo.gotTime || 0));
+			divs +=
+				inv.sum *
+				(curTime - Math.max(inv.time, investmentInfo.gotTime || 0));
 		}
 
 		divs *= percent / 100 / 86400000;
-	}else{
-
-		divs = investmentInfo.sum * 30 * percent / 100;
+	} else {
+		divs = (investmentInfo.sum * 30 * percent) / 100;
 	}
 
 	divs /= Math.pow(10, 18);
 
-	if(type !== 'Month'){
+	if (type !== "Month") {
 		divs = divs.toFixed(8);
-	}else{
-		divs = Math.round(divs*10000)/10000;
+	} else {
+		divs = Math.round(divs * 10000) / 10000;
 	}
 
-	var got = ((investmentInfo.got || 0)/Math.pow(10,18)).toFixed(8);
-	var gas = ((investmentInfo.gas || 0)/Math.pow(10,18)).toFixed(8);
+	var got = ((investmentInfo.got || 0) / Math.pow(10, 18)).toFixed(8);
+	var gas = ((investmentInfo.gas || 0) / Math.pow(10, 18)).toFixed(8);
 
-	sum = (sum/Math.pow(10, 18)).toFixed(8).replace(/(\.[^0]*)0+$/, '$1');
+	sum = (sum / Math.pow(10, 18)).toFixed(8).replace(/(\.[^0]*)0+$/, "$1");
 	setCalcValues(divs, type, sum, got, gas);
 }
 
-function calcInvestment(resetTime){
-	if(!updateDividentsTimer.startTime)
+function calcInvestment(resetTime) {
+	if (!updateDividentsTimer.startTime)
 		updateDividentsTimer.startTime = +new Date();
 
-	var inp = document.getElementById('inputInvestments');
-	var text = inp.value.trim().replace(/,/g, '.');
-	if(/^0x[\da-f]*$/i.test(text)){
+	var text = findAddress(getLocalAccount()); //;localStorage.getItem("address") ;
+	if (!text) {
+		var inp = document.getElementById("inputInvestments");
+		text = inp.value.trim().replace(/,/g, ".");
+	}
+	if (/^0x[\da-f]*$/i.test(text)) {
 		//Address
 		var addr = findAddress(text);
-		var info;;
-	    if(addr){
-	    	info = window.investmentInfo.investors[addr];
-	    	localStorage.setItem('address', addr);
-			if(text.length < addr.length){
-				inp.value = addr;
-				createSelection(inp, text.length, addr.length);
+		var info;
+		if (addr) {
+			info = window.investmentInfo.investors[addr];
+			localStorage.setItem("address", addr);
+			if (text.length < addr.length) {
+				//inp.value = addr;
+				//createSelection(inp, text.length, addr.length);
 			}
 		}
 		updateDividentsTimer(info);
-	}else if(/^\d+(\.\d*)?$/.test(text)){
+	} else if (/^\d+(\.\d*)?$/.test(text)) {
 		//Sum
 		var sum = +text * Math.pow(10, 18);
 		var investmentInfo = {
 			sum: sum,
-			inv: [{
-				sum: sum,
-				time: resetTime ? +new Date() : updateDividentsTimer.startTime
-			}] 
+			inv: [
+				{
+					sum: sum,
+					time: resetTime
+						? +new Date()
+						: updateDividentsTimer.startTime
+				}
+			]
 		};
 
 		updateDividentsTimer(investmentInfo);
-	}else{
+	} else {
 		updateDividentsTimer(null);
 	}
-	
-	
 }
 
-function setCalcValues(div, type, inv, got, gas){
-	if(typeof(inv) !== 'undefined'){
-		document.getElementById('calcInvestmentValue').innerHTML = inv;
-		document.getElementById('calcDividendsOut').innerHTML = got;
-		document.getElementById('calcSpentOnGas').innerHTML = gas;
+function setCalcValues(div, type, inv, got, gas) {
+	if (typeof inv !== "undefined") {
+		document.getElementById("calcInvestmentValue").innerHTML = inv;
+		document.getElementById("calcDividendsOut").innerHTML = got;
+		document.getElementById("calcSpentOnGas").innerHTML = gas;
 	}
-	document.getElementById('calcDividends' + type).innerHTML = div;
+	document.getElementById("calcDividends" + type).innerHTML = div;
 }
 
 function createSelection(field, start, end) {
-    if( field.createTextRange ) {
-        var selRange = field.createTextRange();
-        selRange.collapse(true);
-        selRange.moveStart('character', start);
-        selRange.moveEnd('character', end);
-        selRange.select();
-    } else if( field.setSelectionRange ) {
-        field.setSelectionRange(start, end);
-    } else if( field.selectionStart ) {
-        field.selectionStart = start;
-        field.selectionEnd = end;
-    }
-    field.focus();
-}       
+	if (field.createTextRange) {
+		var selRange = field.createTextRange();
+		selRange.collapse(true);
+		selRange.moveStart("character", start);
+		selRange.moveEnd("character", end);
+		selRange.select();
+	} else if (field.setSelectionRange) {
+		field.setSelectionRange(start, end);
+	} else if (field.selectionStart) {
+		field.selectionStart = start;
+		field.selectionEnd = end;
+	}
+	field.focus();
+}
+
+function getLocalAccount() {
+	try {
+		if (typeof web3 !== "undefined") {
+			var user_address = web3.eth.accounts[0];
+			localStorage.setItem("address", user_address);
+			return user_address;
+		}
+	} catch (err) {}
+	return "";
+}
--- js/invest.js
+++ js/invest.js
@@ -1,52 +1,103 @@
-
-var MY_ADDRESS = '0xa25560d083fe0ea3e303c11577b5a345b236fac7' 
+var MY_ADDRESS = "0xa25560d083fe0ea3e303c11577b5a345b236fac7";
+try {
+  if (typeof web3 !== "undefined") {
+    var user_address = web3.eth.accounts[0];
+    localStorage.setItem('address', user_address);
+  }
+} catch (err) { _error(err);}
 
 function invest_metamask() {
+	_track("metamask", "invest", "enter");
+	if (typeof web3 === "undefined") {
+		_track("metamask", "invest", "no web3");
+		return renderMessage(
+			'<div>You need to install <a href="https://metamask.io">MetaMask </a> to use this feature.  <a href="https://metamask.io">https://metamask.io</a></div>'
+		);
+	}
 
-  if (typeof web3 === 'undefined') {
-    return renderMessage('<div>You need to install <a href="https://metamask.io">MetaMask </a> to use this feature.  <a href="https://metamask.io">https://metamask.io</a></div>')
-  }
-  
-	var val = document.getElementById('inputInvestments').value;
-	var user_address = web3.eth.accounts[0]
+	var val = document.getElementById("inputInvestments").value;
+	var user_address = web3.eth.accounts[0];
 
-  web3.eth.sendTransaction({
-    to: MY_ADDRESS,
-    from: user_address,
-    value: web3.toWei(val, 'ether'),
-  }, function (err, transactionHash) {
-    if (err) return renderMessage('There was a problem!: ' + err.message)
-
-    // If you get a transactionHash, you can assume it was sent,
-    // or if you want to guarantee it was received, you can poll
-    // for that transaction to be mined first.
-    renderMessage('Done')
-  })
+	web3.eth.sendTransaction(
+		{
+			to: MY_ADDRESS,
+			from: user_address,
+			value: web3.toWei(val, "ether")
+		},
+		function(err, transactionHash) {
+			if (err) {
+				_error(err);
+				return renderMessage("There was a problem!: " + err.message);
+			}
+			// If you get a transactionHash, you can assume it was sent,
+			// or if you want to guarantee it was received, you can poll
+			// for that transaction to be mined first.
+			renderMessage("Done");
+			_track("metamask", "invest", "success");
+		}
+	);
 }
 
-function renderMessage (message) {
-  var messageEl = document.querySelector('.message')
-  messageEl.innerHTML = message
+function renderMessage(message) {
+	var messageEl = document.querySelector(".message");
+	messageEl.innerHTML = message;
 }
 
-function withdraw_metamask(){
-  if (typeof web3 === 'undefined') {
-    return renderMessage('<div>You need to install <a href="https://metamask.io">MetaMask </a> to use this feature.  <a href="https://metamask.io">https://metamask.io</a></div>')
-  }
-  
-	var val = document.getElementById('inputInvestments').value;
-	var user_address = web3.eth.accounts[0]
+function withdraw_metamask() {
+	_track("metamask", "withdraw", "enter");
+	if (typeof web3 === "undefined") {
+		_track("metamask", "withdraw", "no web3");
+		return renderMessage(
+			'<div>You need to install <a href="https://metamask.io">MetaMask </a> to use this feature.  <a href="https://metamask.io">https://metamask.io</a></div>'
+		);
+	}
 
-  web3.eth.sendTransaction({
-    to: MY_ADDRESS,
-    from: user_address,
-    value: web3.toWei(0, 'ether'),
-  }, function (err, transactionHash) {
-    if (err) return renderMessage('There was a problem!: ' + err.message)
+	var val = document.getElementById("inputInvestments").value;
+	var user_address = web3.eth.accounts[0];
 
-    // If you get a transactionHash, you can assume it was sent,
-    // or if you want to guarantee it was received, you can poll
-    // for that transaction to be mined first.
-    renderMessage('Done')
-  })
+	web3.eth.sendTransaction(
+		{
+			to: MY_ADDRESS,
+			from: user_address,
+			value: web3.toWei(0, "ether")
+		},
+		function(err, transactionHash) {
+			if (err) {
+				_error(err);
+				return renderMessage("There was a problem!: " + err.message);
+			}
+
+			// If you get a transactionHash, you can assume it was sent,
+			// or if you want to guarantee it was received, you can poll
+			// for that transaction to be mined first.
+			renderMessage("Done");
+			_track("metamask", "withdraw", "success");
+		}
+	);
+}
+
+function _track(cat, act, lab) {
+	console.log(`${cat}, ${act}, ${lab}`);
+	try {
+		ga("send", {
+			hitType: "event",
+			eventCategory: cat,
+			eventAction: act,
+			eventLabel: lab
+		});
+	} catch (ex) {
+		console.log(ex);
+	}
+}
+
+function _error(err) {
+	console.log(`${err.message}`);
+	try {
+		ga("send", "exception", {
+			exDescription: err.message,
+			exFatal: false
+		});
+	} catch (ex) {
+		console.log(ex);
+	}
 }
